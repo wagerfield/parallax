3d9227100b19e3c7a7f1a89f0c6d7b8c1fb35e04
René Roth <mail@reneroth.org>
2017-02-04 18:18:47 +0100

    use SCSS for examples

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,15 @@
 const gulp = require('gulp')
 const path = require('path')
 
+const autoprefixer = require('autoprefixer')
 const babelify = require('babelify')
 const browserify = require('browserify')
 const browsersync = require('browser-sync').create()
 const buffer = require('vinyl-buffer')
 const notifier = require('node-notifier')
+const postcss = require('gulp-postcss')
 const rimraf = require('rimraf')
+const sass = require('gulp-sass')
 const source = require('vinyl-source-stream')
 const sourcemaps = require('gulp-sourcemaps')
 const uglify = require('gulp-uglify')
@@ -17,17 +20,37 @@ gulp.task('clean', (cb) => {
 })
 
 gulp.task('build', ['clean'], () => {
-  gulp.start('build:js', 'build:js:minified')
+  gulp.start('build:js', 'build:js:minified', 'build:scss')
 })
 
 function showError(arg) {
   notifier.notify({
-    title: 'JS Error',
+    title: 'Error',
     message: '' + arg,
     sound: 'Basso'
   })
   this.emit('end')
 }
+
+gulp.task('build:scss', () => {
+  return gulp.src(path.join('examples', 'styles.scss'))
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.', 'node_modules'],
+      onError: showError
+    }).on('error', function(error) {
+      showError(error)
+      this.emit('end')
+    }))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['last 2 versions', 'Firefox ESR', 'Explorer >= 9', 'Android >= 4.0', '> 2%']
+      })
+    ]))
+    .pipe(gulp.dest('examples'))
+    .pipe(browsersync.stream({match: '**/*.css'}))
+})
 
 gulp.task('build:js', () => {
   return browserify({entries: path.join('src', 'parallax.js'), debug: true})
@@ -65,6 +88,7 @@ gulp.task('watch', ['build'], () => {
   })
 
    gulp.watch(path.join('src', 'parallax.js'), ['build:js', 'build:js:minified'])
+    gulp.watch(path.join('examples', 'styles.scss'), ['build:scss'])
 })
 
 gulp.task('default', ['build'])
