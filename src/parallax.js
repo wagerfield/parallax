43c11f5370986557dcf8732d859534be6d03bdcb
René Roth <mail@reneroth.org>
2017-05-01 17:12:32 +0200

    Insert helper functions into main JS file to allow for easier manual copy of source file

--- src/parallax.js
+++ src/parallax.js
@@ -7,7 +7,122 @@
 */
 
 import rqAnFr from 'raf'
-import helpers from './helpers.js'
+
+const helpers = {
+  propertyCache: {},
+  vendors: [null,['-webkit-','webkit'],['-moz-','Moz'],['-o-','O'],['-ms-','ms']],
+
+  clamp(value, min, max) {
+    return min < max
+      ? (value < min ? min : value > max ? max : value)
+      : (value < max ? max : value > min ? min : value)
+  },
+
+  data(element, name) {
+    return helpers.deserialize(element.getAttribute('data-'+name))
+  },
+
+  deserialize(value) {
+    if (value === 'true') {
+      return true
+    } else if (value === 'false') {
+      return false
+    } else if (value === 'null') {
+      return null
+    } else if (!isNaN(parseFloat(value)) && isFinite(value)) {
+      return parseFloat(value)
+    } else {
+      return value
+    }
+  },
+
+  camelCase(value) {
+    return value.replace(/-+(.)?/g, (match, character) => {
+      return character ? character.toUpperCase() : ''
+    })
+  },
+
+  accelerate(element) {
+    helpers.css(element, 'transform', 'translate3d(0,0,0)')
+    helpers.css(element, 'transform-style', 'preserve-3d')
+    helpers.css(element, 'backface-visibility', 'hidden')
+  },
+
+  transformSupport(value) {
+    let element = document.createElement('div'),
+        propertySupport = false,
+        propertyValue = null,
+        featureSupport = false,
+        cssProperty = null,
+        jsProperty = null
+    for (let i = 0, l = helpers.vendors.length; i < l; i++) {
+      if (helpers.vendors[i] !== null) {
+        cssProperty = helpers.vendors[i][0] + 'transform'
+        jsProperty = helpers.vendors[i][1] + 'Transform'
+      } else {
+        cssProperty = 'transform'
+        jsProperty = 'transform'
+      }
+      if (element.style[jsProperty] !== undefined) {
+        propertySupport = true
+        break
+      }
+    }
+    switch(value) {
+      case '2D':
+        featureSupport = propertySupport
+        break
+      case '3D':
+        if (propertySupport) {
+          let body = document.body || document.createElement('body'),
+              documentElement = document.documentElement,
+              documentOverflow = documentElement.style.overflow,
+              isCreatedBody = false
+
+          if (!document.body) {
+            isCreatedBody = true
+            documentElement.style.overflow = 'hidden'
+            documentElement.appendChild(body)
+            body.style.overflow = 'hidden'
+            body.style.background = ''
+          }
+
+          body.appendChild(element)
+          element.style[jsProperty] = 'translate3d(1px,1px,1px)'
+          propertyValue = window.getComputedStyle(element).getPropertyValue(cssProperty)
+          featureSupport = propertyValue !== undefined && propertyValue.length > 0 && propertyValue !== 'none'
+          documentElement.style.overflow = documentOverflow
+          body.removeChild(element)
+
+          if ( isCreatedBody ) {
+            body.removeAttribute('style')
+            body.parentNode.removeChild(body)
+          }
+        }
+        break
+    }
+    return featureSupport
+  },
+
+  css(element, property, value) {
+    let jsProperty = helpers.propertyCache[property]
+    if (!jsProperty) {
+      for (let i = 0, l = helpers.vendors.length; i < l; i++) {
+        if (helpers.vendors[i] !== null) {
+          jsProperty = helpers.camelCase(helpers.vendors[i][1] + '-' + property)
+        } else {
+          jsProperty = property
+        }
+        if (element.style[jsProperty] !== undefined) {
+          helpers.propertyCache[property] = jsProperty
+          break
+        }
+      }
+    }
+    element.style[jsProperty] = value
+  }
+
+}
 
 const MAGIC_NUMBER = 30,
       DEFAULTS = {
